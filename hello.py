4e5a1887770f1fc0dfd6aad488b7ba736a766354
Yan Wei Kun <24013205g@connect.polyu.hk>
2024-09-14 07:31:18 +0000

    funny copilot

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hellow World")
+# print("Hellow World")
+
+def print_integers(n):
+    if n < 0:
+        raise ValueError("Input must be a positive integer")
+    for i in range(n + 1):
+        print(i)
+
+print_integers(10)
